00cf43eed3e8112c3149577bd66a4af79d41cf4b
rajeshvai <imrajesh2005@gmail.com>
2024-12-20 10:08:42 +0545

    first commit

--- frontend/src/types/locomotive-scroll.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'locomotive-scroll' {
-  export default class LocomotiveScroll {
-    constructor(options: { el: HTMLElement; smooth: boolean });
-    scrollTo(target: HTMLElement | string, offset?: number, options?: { disableLerp?: boolean }): void;
-    update(): void;
-    destroy(): void;
-  }
-}
